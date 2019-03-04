7112b61d79dd49b6d98c376f7632d673eed3588a
vincent <vjbeltrani@gmail.com>
2019-03-03 21:08:53 -0500

    first checking for DWAVE Quantum Annealer

---
+++ dwave-test.py
@@ -0,0 +1,11 @@
+from dwave.cloud import Client
+from dwave.system.samplers import DWaveSampler
+
+import neal
+
+solver = neal.SimulatedAnnealingSampler()
+
+response = solver.sample_ising({'a': -0.5, 'b': 1.0}, {('a', 'b'): -1}, num_reads=2)
+response.data_vectors['energy']       
+
+print(response)
